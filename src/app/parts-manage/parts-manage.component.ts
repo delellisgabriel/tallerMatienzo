4c7092dc8691da2e35c9c0d74f7e4ebe71a593b4
maximilianoc94 <maximilianoc94@gmail.com>
2018-01-29 17:04:19 -0400

    boton de menu

--- src/app/parts-manage/parts-manage.component.ts
+++ src/app/parts-manage/parts-manage.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $: any;
+
 @Component({
   selector: 'app-parts-manage',
   templateUrl: './parts-manage.component.html',
@@ -10,6 +12,30 @@ export class PartsManageComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+
+    $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 300
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true, // Choose whether you can drag to open on touch screens,
+    });
+    $('.parallax').parallax();
+
+    $('.datepicker').pickadate({
+      selectMonths: true, // Creates a dropdown to control month
+      selectYears: 15, // Creates a dropdown of 15 years to control year,
+      today: 'Hoy',
+      clear: 'Limpiar',
+      close: 'Ok',
+      closeOnSelect: false // Close upon selecting a date,
+    });
+
+    $('.modal').modal();
+    $('select').material_select();
+
   }
 
 }
