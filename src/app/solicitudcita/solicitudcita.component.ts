d4d7d17c132f9dcc84f1017592603461ef7904ac
Brablawaxplagabrwabla <32771607+Brablawaxplagabrwabla@users.noreply.github.com>
2018-02-20 16:20:14 -0400

    Commit a mano

--- src/app/solicitudcita/solicitudcita.component.ts
+++ src/app/solicitudcita/solicitudcita.component.ts
@@ -32,20 +32,22 @@ export class SolicitudcitaComponent implements OnInit, AfterViewInit {
   }
 
   solicitarCita() {
-    this.solicitud.idUsuario = this.authService.getUser()["idUsuario"];
-    console.log(this.solicitud);
-
-    /*
+    if (this.solicitud.idAux) {
+      this.solicitud.idVehiculo = Number.parseInt(this.solicitud.idAux);
+      delete this.solicitud.idAux;
+    }
     this.solicitud.idUsuario = this.authService.getUser()['idUsuario'];
-    console.log(this.solicitud); //Cuando esta vaina bote el objeto que es. Podemos descomentar
-    
-    
-    this.database.addThis('ModeloCitas', this.vehiculoSelected).then((result) => {
+    this.solicitud.fecha_inicial = this.database.dateFormatter(this.solicitud.fecha_final);
+    this.solicitud.fecha_final = this.database.dateFormatter(this.solicitud.fecha_final);
+
+    console.log(this.solicitud); // Cuando esta vaina bote el objeto que es. Podemos descomentar
+
+    /* this.database.addThis('ModeloCitas', this.vehiculoSelected).then((result) => {
       console.log(result); //Esto deberia botar True
       this.router.navigate(['/mycars', this.authService.getUser()["idUsuario"]];
     });
     */
-    
+
   }
 
   ngOnInit() {
@@ -61,15 +63,6 @@ export class SolicitudcitaComponent implements OnInit, AfterViewInit {
       draggable: true, // Choose whether you can drag to open on touch screens,
     });
     $('.parallax').parallax();
-
-    $('.datepicker').pickadate({
-      selectMonths: true, // Creates a dropdown to control month
-      selectYears: 15, // Creates a dropdown of 15 years to control year,
-      today: 'Hoy',
-      clear: 'Limpiar',
-      close: 'Ok',
-      closeOnSelect: false // Close upon selecting a date,
-    });
 
     $('.modal').modal();
     $('select').material_select();
