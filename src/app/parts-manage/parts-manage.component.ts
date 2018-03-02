0578829429392d13dab84b974b2ce3ea95faf28d
maximilianoc94 <maximilianoc94@gmail.com>
2018-03-02 19:30:36 -0400

    cambios de part-manage

--- src/app/parts-manage/parts-manage.component.ts
+++ src/app/parts-manage/parts-manage.component.ts
@@ -24,6 +24,7 @@ export class PartsManageComponent implements OnInit {
   buscar = {
     Marca: "",
     Modelo: "",
+    Nombre: "",
   }
 
   constructor(private auth: AuthService, private database: DatabaseService) { }
