b3b55c3852e1ce9f4d1c6c466bca69c5fe68eff9
brablawaxplagabrwabla <gtroncone@correo.unimet.edu.ve>
2018-03-10 18:42:38 -0400

    HTML en emailService y lectura de QR completa

--- src/app/qrService/qr.service.ts
+++ src/app/qrService/qr.service.ts
@@ -14,6 +14,7 @@ export class QrService {
   private base = 'http://localhost:3000/qr';
   private base1 = 'https://api.qrserver.com/v1/create-qr-code/?data=';
   private base2 = '&size=';
+  private base3 = 'http://api.qrserver.com/v1/read-qr-code/?fileurl=';
   private password = 'z9>nV?:"&)~4*d_T[6k{T3wy2;.#Vd*+';
 
   constructor(private http: HttpClient) { }
@@ -24,6 +25,16 @@ export class QrService {
         base: this.base1 + datos + this.base2 + tamano + 'x' + tamano,
         password: this.password,
         operacion: 'crear'
+      }).toPromise();
+  }
+
+  leerQR(fotoBase64: string) {
+    this.http.post(this.base,
+      {
+        foto: fotoBase64,
+        password: this.password,
+        operacion: 'leer',
+        base: this.base3
       }).toPromise();
   }
 }
