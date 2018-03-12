4fe650e8c50806df6136f8500e81854b8384aa4b
Gabriel De Lellis <delellisgabriel@gmail.com>
2018-03-12 11:41:50 -0300

    cambios canvas, sigue bugs bunny

--- src/app/car-canvas/car-canvas.component.ts
+++ src/app/car-canvas/car-canvas.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewChild } from '@angular/core';
 import { CanvasWhiteboardComponent } from 'ng2-canvas-whiteboard';
 import { CanvasWhiteboardOptions } from 'ng2-canvas-whiteboard/dist/canvas-whiteboard.component';
 
@@ -10,10 +10,13 @@ import { CanvasWhiteboardOptions } from 'ng2-canvas-whiteboard/dist/canvas-white
 })
 export class CarCanvasComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  @Output() onSave = new EventEmitter<string | Blob>();
 
   canvasOptions: CanvasWhiteboardOptions = {
     drawButtonEnabled: true,
@@ -34,5 +37,30 @@ export class CarCanvasComponent implements OnInit {
     shouldDownloadDrawing: true,
     imageUrl: "http://img.turbo.fr/04353116-photo-bmw-serie-6-gran-coupe-premiers-croquis.jpg",
   };
+
+  @ViewChild('canvasWhiteboard') canvasWhiteboard: CanvasWhiteboardComponent;
+
+  onCanvasUndo(updateUUID: string) {
+    console.log(`UNDO with uuid: ${updateUUID}`);
+
+    //Returns base64 string representation of the canvas
+    let generatedString = this.canvasWhiteboard.generateCanvasDataUrl("image/jpeg", 0.3);
+
+    //Generates a IE canvas blob using a callbak method
+    this.canvasWhiteboard.generateCanvasBlob((blob: any) => {
+      console.log(blob);
+    }, "image/png");
+
+    //This method uses both of the above method and returns either string or blob
+    //using a callback method
+    this.canvasWhiteboard.generateCanvasData((generatedData: string | Blob) => {
+      console.log(generatedData);
+    }, "image/png", 1);
+
+    //If you need the context of the canvas
+    let context = this.canvasWhiteboard.context;
+  }
+
+
 
 }
